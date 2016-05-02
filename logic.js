3bb2be5ebb2451f0e008c13b0bb06907410c5f30
Alex Burrell <alexburrell@Alexs-MacBook.local>
2016-05-02 12:13:08 -0700

    Fixing minutes logic

--- logic.js
+++ logic.js
@@ -9,9 +9,9 @@ function updateTime() {
         seconds = now.getSeconds();
 
     var thaiHour = time["hours"][hour]["thai"],
-        tens = Math.floor(minutes/10),
-        thaiMinutesTens = time["minutes"]["tens"][tens]["thai"],
-        thaiMinutesOnes = time["minutes"]["ones"][minutes-tens]["thai"];
+        tens = Math.floor(minutes/10)*10,
+        thaiMinutesTens = time["minutes"][tens]["thai"],
+        thaiMinutesOnes = time["minutes"][minutes-tens]["thai"];
 
     if (minutes < 10) minutes = "0" + minutes;
     if (seconds < 10) seconds = "0" + seconds;
